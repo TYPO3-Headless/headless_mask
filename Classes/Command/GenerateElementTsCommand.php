d733accd97b1cdb48f81ca0db263f6fb7646ad21
Nilesh Malankiya <nilesh@nitsantech.com>
2023-10-22 10:59:51 +0530

    :art: Improved the file creation logic

--- Classes/Command/GenerateElementTsCommand.php
+++ Classes/Command/GenerateElementTsCommand.php
@@ -19,12 +19,23 @@ class GenerateElementTsCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $maskUtility = GeneralUtility::makeInstance(MaskElementsUtility::class);
+
+        //Method to call prepared the elements TypoScript...
         $tsComponents = $maskUtility->setupComponentWiseTypoScript();
-        $typoScriptFilePath = Environment::getPublicPath() . '/typo3conf/ext/ns_headless_mask/Configuration/TypoScript/setup.typoscript';
+
+        $typoScriptFileDir = Environment::getPublicPath() . '/typo3conf/ext/ns_headless_mask/Configuration/TypoScript/';
         if (Environment::isComposerMode()) {
-            $typoScriptFilePath = Environment::getProjectPath() . '/vendor/nitsan/ns-headless-mask/Configuration/TypoScript/setup.typoscript';
+            $typoScriptFileDir = Environment::getProjectPath() . '/vendor/nitsan/ns-headless-mask/Configuration/TypoScript/';
         }
 
+        //Check if directory is available or not, if not then it will be created
+        if (!file_exists($typoScriptFileDir)) {
+            GeneralUtility::mkdir_deep($typoScriptFileDir);
+        }
+
+        $typoScriptFilePath = $typoScriptFileDir.'setup.typoscript';
+
+        //File created...
         GeneralUtility::writeFile($typoScriptFilePath, $tsComponents);
         return Command::SUCCESS;
     }
